f4f7501ca4d77840ab1615410e725c8dd047c3df
Andrew Lu <awclu@ucdavis.edu>
2020-02-09 23:08:32 -0800

    working on local

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wholesomeScrape"
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/wholesomeScrape2"
 
 // Connect to the Mongo DB
 mongoose.connect(MONGODB_URI);
@@ -56,7 +56,7 @@ axios.get("https://old.reddit.com/r/wholesomememes/").then(function(response) {
     db.Article.create(result)
       .then(function(dbArticle) {
         // View the added result in the console
-        console.log(dbArticle);
+        console.log("line 59: "+ dbArticle);
       })
       .catch(function(err) {
         // If an error occurred, log it
@@ -98,22 +98,16 @@ app.get("/articles/:id", function(req, res) {
     });
 });
 
-// Route for saving/updating an Article's associated Note
+
 app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
   db.Note.create(req.body)
     .then(function(dbNote) {
-      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
     })
     .then(function(dbArticle) {
-      // If we were able to successfully update an Article, send it back to the client
       res.json(dbArticle);
     })
     .catch(function(err) {
-      // If an error occurred, send it to the client
       res.json(err);
     });
 });
